05fd68d8da94e9b06fcb34836bed2f26baa5b8fb
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-03-03 21:29:59 -0800

    added some more files -AP

---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const Tasks: Task[] = [
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'May 5th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Goose Appointment',
+        day: 'May 6th at 2:30PM',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Caboose Appointment',
+        day: 'May 7th at 2:30PM',
+        reminder: false
+    }
+]
