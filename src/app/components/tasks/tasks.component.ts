5dff3667aba6164506086db918ba69ba628e93b5
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2024-03-03 21:41:31 -0800

    added service function calls that returns back a list of tasks from the file, acting as a backend -AP

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { Tasks } from '../mock-task';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../Task';
 import { CommonModule } from '@angular/common';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskService } from '../../services/task.service';
 @Component({
   selector: 'app-tasks',
   standalone: true,
@@ -10,7 +10,15 @@ import { TaskItemComponent } from '../task-item/task-item.component';
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   //Here we are pulling from the tasks folder the three tasks we have. Then we are going to be importing them and inserting them as a property. Which will be looped over in the html file.
-  tasks: Task[] = Tasks;
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService){
+
+  }
+
+  ngOnInit():void{
+    //usually we want to use obserables for getting from server, but this is a good example.
+    this.tasks = this.taskService.getTasks()
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { Tasks } from '../components/mock-task';
+import { Task } from '../components/Task';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
   constructor() { }
+
+  getTasks():Task[]{
+    return Tasks
+  }
 }
